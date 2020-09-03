0239e1a95f4ef6fd9a75dcd505d1bbe03f301f83
echonabin <echonabin@gmail.com>
2020-09-04 04:48:30 +0545

    Loading added on home page

--- src/container/Body/pages/Home/Home.js
+++ src/container/Body/pages/Home/Home.js
@@ -1,48 +1,53 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import { useContext } from "react";
 
 import VideoCard from "../Videos/VideoCard";
 import HomeLayout from "./HomeLayout";
 import HeadingButton from "../../../../components/Buttons/HeadingButton";
 import FeaturedBlog from "../Blogs/Featured/FeaturedBlog";
-import dummyblog from "../Blogs/dummyblog.json";
 import "../Blogs/Blogs.css";
 import LoadMore from "../../../../components/Buttons/LoadMore";
-
-const blogs = dummyblog.filter(
-  (featuredBlog) => featuredBlog.featured === true
-);
+import BlogContext from "../../../../context/blogs/BlogContext";
+import Loading from "../../../../components/Loading/Loading";
 const Home = () => {
-  return (
-    <>
-      <HomeLayout>
-        <HeadingButton text='Featured Blogs' />
-        <div className='band'>
-          {blogs.map((blog) => (
-            <FeaturedBlog
-              key={blog.id}
-              item={blog.item}
-              header={blog.header}
-              headlines={blog.headlines}
-              author={blog.author}
-              thumbnail={blog.thumbnail}
-              data={blog}
-            />
-          ))}
-        </div>
-        <br />
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}>
-          <LoadMore pageUrl='/blogs' Title='See More' />
-        </div>
-        <HeadingButton text='Featured Videos' />
-      </HomeLayout>
-    </>
-  );
+  const data = useContext(BlogContext);
+  const blogsArray = data.blogs.data;
+  if (!blogsArray) {
+    return <Loading />;
+  } else {
+    const filterBlogs = blogsArray.filter((blog) => blog.Featured === true);
+    return (
+      <>
+        <HomeLayout>
+          <HeadingButton text='Featured Blogs' />
+          <div className='band'>
+            {filterBlogs.map((blog) => (
+              <FeaturedBlog
+                key={blog.id}
+                item={blog.CardPosition}
+                header={blog.title}
+                headlines={blog.title}
+                author={blog.created_by.firstname}
+                thumbnail={blog.thumbnails.url}
+                data={blog}
+              />
+            ))}
+          </div>
+          <br />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}>
+            <LoadMore pageUrl='/blogs' Title='See More' />
+          </div>
+          <HeadingButton text='Featured Videos' />
+        </HomeLayout>
+      </>
+    );
+  }
 };
 
 export default Home;
