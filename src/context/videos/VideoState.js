3f56196c8e35bca80d4d61107957c523c03613ce
echonabin <echonabin@gmail.com>
2020-09-04 19:14:51 +0545

    On context, new playlist of upload added for all videos!!

--- src/context/videos/VideoState.js
+++ src/context/videos/VideoState.js
@@ -1,41 +1,27 @@
 import React, { Component } from "react";
 
-import ftVideoId from "../../api/blogApi";
 import VideoContext from "./VideosContext";
 import youtube from "../../api/youtubeApi";
 export class VideoState extends Component {
   state = {
     videos: [],
-    videosId: [],
+    featuredVideo: [],
     selectedVideo: null,
     openModal: false,
   };
   async componentDidMount() {
-    const params = new URLSearchParams();
-    params.append("part", "snippet");
-    params.append("part", "statistics");
-    params.append("id", ["8KbUubuELPs", "2b8xPJLQWUw"]);
-    params.append("key", "AIzaSyAuR63mqhbH0EVd73070DpRASuODidLEiQ");
-    const request = {
-      params: params,
-    };
-    const response = await youtube.get("/videos", request);
+    const response = await youtube.get("/playlistItems", {
+      params: {
+        part: "snippet",
+        playlistId: "UUZ9-B7ebuPUc3zfSu7TaDKw",
+        key: "AIzaSyAuR63mqhbH0EVd73070DpRASuODidLEiQ",
+        maxResults: "16",
+      },
+    });
     this.setState({
       videos: response.data,
     });
-    const Idresponse = await ftVideoId.get("/featured-videos");
-    const videoIdArr = Idresponse.data;
-    console.log(videoIdArr);
-    videoIdArr.map((videoId) => {
-      const ftVideoId = videoId.VideoID;
-      const arr = ftVideoId.split();
-      const newArray = this.state.videosId.concat(arr);
-      this.setState({
-        videosId: newArray,
-      });
-      console.log(arr);
-    });
-    console.log(this.state.videosId);
+    console.log(this.state.videos);
   }
   render() {
     return (
