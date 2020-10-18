b8a8f1c30553ad59a5f686d1124685bc857ff7a2
echonabin <echonabin@gmail.com>
2020-10-18 15:48:20 +0545

    loading added

--- src/container/Body/pages/Blogs/Place/Place.js
+++ src/container/Body/pages/Blogs/Place/Place.js
@@ -5,13 +5,14 @@ import HeadingButton from "../../../../../components/Buttons/HeadingButton";
 import PageContainer from "../Layout/Page";
 import TrendingCard from "../../../../../components/trending/TrendingCard";
 import {withRouter} from 'react-router-dom'
+import Loading from "../../../../../components/Loading/Loading";
 
 class Place extends Component {
   static contextType = BlogsContext;
   render() {
     const { state } = this.context;
     if (state.isLoading) {
-      return <h1>Loading.....</h1>;
+      return <Loading />;
     }
     const renderedList = state.blogs.data.map((blog) => {
       if (blog.categories.includes(2)) {
